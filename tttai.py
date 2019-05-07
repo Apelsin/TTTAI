d9a4dfbc7ee2df86f75f1b7e43125da887f37fc4
Apelsin <appelsindrikken@gmail.com>
2019-05-07 04:35:43 -0700

    self game

--- tttai.py
+++ tttai.py
@@ -7,28 +7,26 @@ Tic-Tac-Toe AI runner
 import numpy as np
 from tictactoe import State, Mark
 from tictactoe.cache import StateCache
-from tictactoe.ai import cache_state_desirability, calculate_next_state_for
-
+# from tictactoe.ai import cache_state_desirability
+from tictactoe.ai import calculate_next_state_for
+import time
 
 def main():
     cache = StateCache()
     cache.load('state-cache.json')
-    state = State(np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]]))
-    print(state)
-    state = calculate_next_state_for(cache, state, Mark.XMARK)
-    print(state)
-    state.set_mark(1, 1, Mark.OMARK)
-    print(state)
-    state = calculate_next_state_for(cache, state, Mark.XMARK)
-    print(state)
-    state.set_mark(2, 2, Mark.OMARK)
-    print(state)
-    state = calculate_next_state_for(cache, state, Mark.XMARK)
-    print(state)
-    state.set_mark(0, 1, Mark.OMARK)
-    print(state)
-    state = calculate_next_state_for(cache, state, Mark.XMARK)
-    print(state)
+    mark = Mark.OMARK
+    states = [State()]
+
+    start_time = time.time()
+    while states[-1].winner is None:
+        states.append(calculate_next_state_for(cache, states[-1], mark))
+        mark = Mark.get_next(mark)
+    milliseconds = round(1000 * (time.time() - start_time))
+    print('Calculated', len(states) - 1, "moves in", milliseconds, "ms")
+
+    for state in states:
+        print()
+        print(state)
 
     # cache.clear_desirability()
     # cache_state_desirability(cache, State(), Mark.OMARK)
